cbbbfd52d8f81320e2530108ca0b47f322c2c7bf
James Connor <jamesegconnor@gmail.com>
2020-11-27 00:37:09 -0500

    Row Range Option
    - Modified data-refiner.css: Added styling for options within the data refiner.
    - Modified data-refiner.js: Added functions that support row ranges to permit a user to limit the length of the dataset that they choose. Also made it so that empty rows are removed.
    - Modified stylesheet.css: Changed up some colors.

--- js/data-refiner.js
+++ js/data-refiner.js
@@ -12,13 +12,18 @@ function loadDatasets(fileContents) {
     $("div[name=data-refiner] .panel div.actions button[name=export]").remove();
     
     //Create the form for selecting options.
-    var form = $("<form class='file-headers'><h3>Form Headers</h3><ul name='checkboxes'></ul></form><div class='buttons flex-row-center'></div>");
-    $("div[name=data-refiner] .panel").prepend(form)
+    var form = $("<form class='file-headers'><h3>Form Headers</h3><ul name='checkboxes'></ul></form></div>");
+    $("div[name=data-refiner] .panel").prepend(form);
     
     //Loop through each file, save its contents, and add its fields to the list of choices.
     for(var i = 0; i < fileContents.length; i++) {
+        fileContents[i] = fileContents[i].split('\n').filter(x => {
+            return x.replace(/,|\r|\n/g, '').length != 0;
+        }).join('\n');
+        
         var data = Papa.parse(fileContents[i], {
-            header: true
+            header: true,
+            skipEmptyLines: true
         });
         contentObjs.push(data.data);
                 
@@ -36,22 +41,54 @@ function loadDatasets(fileContents) {
     }
     
     allOrNoneButtons();
+    rowSelectorOption();
     exportButton();
+}
+
+//Creates an option to choose a row range to export
+function rowSelectorOption() {
+    //A variable tracking the maximum number of rows allowed.
+    var maxRows = 0;
+    contentObjs.forEach(x => {
+        maxRows += x.length;
+    });
+    
+    //Create the element
+    $("form.file-headers").append($("<h3>Options</h3><div class='option flex-row' name='rows'><input type='checkbox' name='select-rows'><p>Select Rows</p><input type='number' step='1' name='select-rows-start' value='0'><p>to</p><input type='number' step='1' name='select-rows-end' value='" + maxRows + "'></div>"));
+    
+    //Stop non-numerical values from being entered
+    $("form.file-headers .option[name=rows] input[type=number]")
+    .on("input", function() {
+        $(this).val($(this).val().replace(/[^0-9]/g,''));
+    });
+    
+    //Stop values over the maximum row count from being entered.
+    $("form.file-headers .option[name=rows] input[name=select-rows-end]")
+    .on("change", function() {
+        $(this).val($(this).val().replace(/[^0-9]/g,''));
+        
+        if(parseInt($(this).val()) > maxRows)
+            $(this).val(maxRows);
+    })
 }
 
 //Add buttons for selecting all or none.
 function allOrNoneButtons() {
+    //Element for holding the buttons
+    var buttons = $("<div class='buttons flex-row-center'>");
+    $("form.file-headers").append(buttons);
+    
     //Create the elements
-    var selectAllButton = $("<button type='button' name='select-all' class='alt-button'>Select All</button>").click(function() {
+    var selectAllButton = $("<button type='button' name='select-all' class='alt-button flex-column'>Select All</button>").click(function() {
         $("form.file-headers ul[name=checkboxes] li input[type=checkbox]").prop("checked", true);
     });
-    var selectNoneButton = $("<button type='button' name='select-none' class='alt-button'>Select None</button>").click(function() {
+    var selectNoneButton = $("<button type='button' name='select-none' class='alt-button flex-column'>Select None</button>").click(function() {
         $("form.file-headers ul[name=checkboxes] li input[type=checkbox]").prop("checked", false);
     });
     
     //Append them
-    $("form.file-headers").next(".buttons").append(selectAllButton);
-    $("form.file-headers").next(".buttons").append(selectNoneButton);
+    buttons.append(selectAllButton);
+    buttons.append(selectNoneButton);
 }
 
 //A function for creating the export button
@@ -64,6 +101,14 @@ function exportButton() {
 
 //A function for exporting the selection
 function exportFiles() {
+    var blob = new Blob([
+        Papa.unparse(generateExportedCSVObject())
+    ], {type: "text/csv;charset=utf-8"});
+    saveAs(blob, "export.csv");
+}
+
+//Generates a CSV file with only the selected fields included.
+function generateExportedCSVObject() {
     //Get all checked inputs to get a list of the selected fields.
     var selectedFields = $("form.file-headers ul[name=checkboxes] input:checked").map(function() { 
         return this.value; 
@@ -71,13 +116,27 @@ function exportFiles() {
     
     var result = [selectedFields];
     
+    //Get the start and end rows.
+    var rowsOption = $("form.file-headers .option[name=rows] input[type=checkbox]").prop("checked");
+    var bounds = rowsOption ? [
+        parseInt($("form.file-headers .option[name=rows] input[name=select-rows-start]").val()),
+        parseInt($("form.file-headers .option[name=rows] input[name=select-rows-end]").val())
+    ] : [0, 0];
+    
+    console.log(bounds);
+    
     contentObjs.forEach(content => {
-        for(var i = 0; i < content.length; i++)
+        if(!rowsOption)
+            bounds[1] = content.length;
+        
+        console.log(bounds);
+        
+        for(var i = bounds[0]; i < bounds[1]; i++) {
             result.push(valuesKeysContain(content[i], selectedFields))
+        }
     });
     
-    var blob = new Blob([Papa.unparse(result)], {type: "text/csv;charset=utf-8"});
-    saveAs(blob, "export.csv");
+    return result;
 }
             
 //A function that returns an array of values if their keys exist in the contains array.
